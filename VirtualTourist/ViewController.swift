d4d7e9689866ae33f1a3c6e75e72efa031e605c4
Whatley, Cesar <julceswhat@gmail.com>
2019-09-10 23:12:37 -0400

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Julio Cesar Whatley on 9/10/19.
+//  Copyright © 2019 Capi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
